7a3150dfe90dee3228042efeb04a634ce67f8e91
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-09-24 12:11:35 +0900

    AndroidManifestにNFCとかの許可を追加して、Permissionチェックの処理を追加した

--- android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/MainActivity.java
+++ android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ApplicationHelper.requestPermissions(this, REQUEST_CODE);
     }
 }
