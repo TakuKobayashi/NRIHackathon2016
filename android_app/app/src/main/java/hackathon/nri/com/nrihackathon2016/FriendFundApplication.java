2a3c8e98aad6678ec45eb4e8634e93ef81897f51
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-09-25 03:25:01 +0900

    ざっくりとFacebookログイン処理を入れてみた

--- android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/FriendFundApplication.java
+++ android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/FriendFundApplication.java
@@ -2,11 +2,16 @@ package hackathon.nri.com.nrihackathon2016;
 
 import android.app.Application;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 public class FriendFundApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
 		SocketIOStreamer.getInstance(SocketIOStreamer.class).init(this);
+		FacebookSdk.sdkInitialize(getApplicationContext());
+		AppEventsLogger.activateApp(this);
 	}
 
 	@Override
--- android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/MainActivity.java
+++ android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/MainActivity.java
@@ -1,19 +1,60 @@
 package hackathon.nri.com.nrihackathon2016;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private static int REQUEST_CODE = 1;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ApplicationHelper.requestPermissions(this, REQUEST_CODE);
+
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        // Other app specific specialization
+
+        callbackManager = CallbackManager.Factory.create();
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d(Config.TAG, "success: " + loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d(Config.TAG, "cancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d(Config.TAG, "error:" + exception.getMessage());
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        Log.d(Config.TAG, "result:");
     }
 
     @Override
