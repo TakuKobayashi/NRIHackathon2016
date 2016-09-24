34d35e26d82746f6301b80eea8f4341dc6a0f7d9
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-09-25 04:28:53 +0900

    ひとまずログインした本人の情報は色々と取れたのでこれで締める

--- android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/MainActivity.java
+++ android_app/app/src/main/java/hackathon/nri/com/nrihackathon2016/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 
@@ -48,6 +56,26 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(Config.TAG, "error:" + exception.getMessage());
             }
         });
+        getProfile();
+    }
+
+    private void getProfile(){
+        Log.d(Config.TAG, "login:" + isLoggedIn());
+        if(isLoggedIn()){
+            Profile profile = Profile.getCurrentProfile();
+            Log.d(Config.TAG, "id:" + profile.getId());
+            Log.d(Config.TAG, "fn:" + profile.getFirstName());
+            Log.d(Config.TAG, "mn:" + profile.getMiddleName());
+            Log.d(Config.TAG, "ln:" + profile.getLastName());
+            Log.d(Config.TAG, "name:" + profile.getName());
+            Log.d(Config.TAG, "link:" + profile.getLinkUri());
+            Log.d(Config.TAG, "picture:" + profile.getProfilePictureUri(200,300));
+        }
+    }
+
+    private boolean isLoggedIn() {
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        return accessToken != null;
     }
 
     @Override
